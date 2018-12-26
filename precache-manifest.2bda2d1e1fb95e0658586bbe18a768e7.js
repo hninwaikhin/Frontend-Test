9257c64a01a098a3197998e35aba7c0acb086500
hninwaikhin <wai.khinhninwai@gmail.com>
2018-12-26 21:18:54 +0630

    Updates

---
+++ precache-manifest.2bda2d1e1fb95e0658586bbe18a768e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece25337b6897b94c96f",
+    "url": "/frontend-test/static/js/runtime~main.ece25337.js"
+  },
+  {
+    "revision": "e7bffb4836b98e2d1f7d",
+    "url": "/frontend-test/static/js/main.e7bffb48.chunk.js"
+  },
+  {
+    "revision": "6f5028c5d4169b82fa15",
+    "url": "/frontend-test/static/js/1.6f5028c5.chunk.js"
+  },
+  {
+    "revision": "e7bffb4836b98e2d1f7d",
+    "url": "/frontend-test/static/css/main.c8ef7f6b.chunk.css"
+  },
+  {
+    "revision": "44afef74868910f8e74343d3d5fcbd4e",
+    "url": "/frontend-test/index.html"
+  }
+];
